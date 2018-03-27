51eb3331f3343714b7266d5da7339e68e96c4f91
Youssef <d>
2018-03-27 21:46:20 +0200

    modified SchemaMapper

--- src/structure-map/SchemaMapper.js
+++ src/structure-map/SchemaMapper.js
@@ -5,31 +5,74 @@ const _ = require('lodash');
 
 const BaseMapper = require('./BaseMapper');
 
+const comparators = {
+  equal: function (firstValue, secondValue) {
+    return _.toString(firstValue) === _.toString(secondValue);
+  },
+
+  notEqual: function (firstValue, secondValue) {
+    return _.toString(firstValue) !== _.toString(secondValue);
+  },
+
+  greaterThan: function (firstValue, secondValue) {
+    let result;
+    firstValue = parseInt(firstValue);
+    secondValue = parseInt(secondValue);
+
+    if (_.isNumber(firstValue) && _.isNumber(secondValue)) {
+      result = firstValue > secondValue;
+    } else {
+      result = null;
+    }
+
+    return result;
+  },
+
+  lessThan: function (firstValue, secondValue) {
+    let result;
+    firstValue = parseInt(firstValue);
+    secondValue = parseInt(secondValue);
+
+    if (_.isNumber(firstValue) && _.isNumber(secondValue)) {
+      result = firstValue < secondValue;
+    } else {
+      result = null;
+    }
+
+    return result;
+  }
+}
+
 class SchemaMapper extends BaseMapper {
   constructor(config) {
     super(config);
+    this.comparators = comparators;
   }
 
   date(data, path, currentPath) {
     const { dates } = this.config;
     path = path.replace(dates.head, '');
-    const expressionParts = _.map(path.split(dates.formatter), (item) => {
-      return _.trim(item);
-    });
 
-    const dateValue = this.mapBasedOnSchema(data, _.head(expressionParts), currentPath);
+    let expressionParts = path.split(dates.formatter);
+    expressionParts = _.map(expressionParts, (exp) => _.trim(exp));
 
-    if(_.isNil(dateValue))
+    const dateObjectToCalculate = _.head(expressionParts);
+
+    const dateFormat = _.last(expressionParts);
+
+    const dateValue = this.mapBasedOnSchema(data, dateObjectToCalculate, currentPath);
+
+    if (_.isNil(dateValue))
       return null;
 
-    return moment(dateValue).format(_.last(expressionParts));
+    return moment(dateValue).format(dateFormat);
   }
 
   ifConditions(data, path, currentPath) {
     const expParts = path.match(this.config.conditionRegexs.caseReg);
     let result = null;
 
-    // loop on cases
+    // loop on cases (all if conditions written)
     _.forEach(expParts, (caseExp) => {
       const compResult = this._compare(data, caseExp, currentPath);
       if (compResult) {
@@ -38,7 +81,7 @@ class SchemaMapper extends BaseMapper {
       }
     });
 
-    // if no cases match look for otherwise
+    // if no cases match, look for otherwise
     if (result === null) {
       let otherwise = _.head(path.match(this.config.conditionRegexs.otherwiseReg));
       if (otherwise) {
@@ -53,36 +96,38 @@ class SchemaMapper extends BaseMapper {
   }
 
   concat(data, path, currentPath) {
-    const partsToConcat = _.split(path, this.config.concatination.splitter);
-    const concatParts = _.map(partsToConcat, (valueToCalculate) => {
-      const concatinationData = this._prepareForConcatination(valueToCalculate);
-      concatinationData.itemToCalculate = _.cloneDeep(this.mapBasedOnSchema(data, concatinationData.itemToCalculate, currentPath));
-      return concatinationData;
+    const valuesToConcat = _.split(path, this.config.concatination.splitter);
+
+    const concatParts = _.map(valuesToConcat, (valueToCalculate) => {
+      const concatenationData = this._prepareForConcatenation(valueToCalculate);
+      concatenationData.concatValue = this.mapBasedOnSchema(data, concatenationData.itemToCalculate, currentPath);
+      return concatenationData;
     });
 
     let finalStr = '';
     _.forEach(concatParts, (concatData) => {
-      finalStr += concatData.concatSplitter + concatData.itemToCalculate;
+      finalStr += concatData.joiner + concatData.concatValue;
     });
 
     return _.trim(finalStr);
   }
 
-  _prepareForConcatination(item) {
+  _prepareForConcatenation(item) {
+    item = _.trim(item);
 
-    const concatinationData = {
-      concatSplitter: ' ',
-      itemToCalculate: _.trim(item)
+    const concatenationData = {
+      joiner: ' ',
+      itemToCalculate: item
     };
 
-    if(_.startsWith(_.trim(item), this.config.concatination.customConcat)) {
+    if (_.startsWith(item, this.config.concatination.customConcat)) {
       const start = _.indexOf(item, '\'');
       const end = _.lastIndexOf(item, '\'');
-      concatinationData.concatSplitter = item.substring(start + 1, end);
-      concatinationData.itemToCalculate = _.trim(item.substring(end + 1));
+      concatenationData.joiner = item.substring(start + 1, end);
+      concatenationData.itemToCalculate = _.trim(item.substring(end + 1));
     }
 
-    return concatinationData;
+    return concatenationData;
   }
 
   _compare(data, path, currentPath) {
@@ -90,64 +135,44 @@ class SchemaMapper extends BaseMapper {
       .replace(this.config.conditionRegexs.expValuesReg, '%|%')
       .split('%|%'));
 
-    let firstValue = this.mapBasedOnSchema(data, expValues[0], currentPath);
-    
-    let secondValue = this.mapBasedOnSchema(data, expValues[1], currentPath);
+    const firstValue = this.mapBasedOnSchema(data, expValues[0], currentPath);
+
+    const secondValue = this.mapBasedOnSchema(data, expValues[1], currentPath);
 
     const returnValue = this.mapBasedOnSchema(data, expValues[2], currentPath);
 
-    const comparator = _.head(path.match(this.config.conditionRegexs.comparatorReg));
+    const comparator = this._getComparator(path);
 
-    if (!comparator) {
-      throw new Error('No comparator detected in ' + path);
-    }
-
-    if(_.isNil(firstValue)) {
+    if (_.isNil(firstValue)) {
       return null;
     }
 
     let result = null;
-    switch (comparator) {
-    case '$equal':
-      result = _.toString(firstValue) === _.toString(secondValue);
-      break;
-    case '$not equal':
-      result = _.toString(firstValue) !== _.toString(secondValue);
-      break;
-    case '$greater than':
-      firstValue = parseInt(firstValue);
-      secondValue = parseInt(secondValue);
 
-      if (_.isNumber(firstValue) && _.isNumber(secondValue)) {
-        result = firstValue > secondValue;
-      }
-      else {
-        result = null;
-      }
+    const compareFunction = this.comparators[comparator];
 
-      break;
-    case '$less than':
-      firstValue = parseInt(firstValue);
-      secondValue = parseInt(secondValue);
-
-      if (_.isNumber(firstValue) && _.isNumber(secondValue)) {
-        result = firstValue < secondValue;
-      }
-      else {
-        result = null;
-      }
-
-      break;
-    default:
-      throw new Error(`unsupported comparator >> ${comparator} in the expression >> ${path}`);
+    if (!_.isFunction(compareFunction)) {
+      throw new Error(`unsupported comparator in the expression >> ${path}`);
     }
+
+    result = compareFunction(firstValue, secondValue);
 
     if (result) {
       return returnValue;
     }
   }
 
-  _formatStrArray (strArr) {
+  _getComparator(path) {
+    const comparatorInSchema = _.head(path.match(this.config.conditionRegexs.comparatorReg));
+
+    if (!comparatorInSchema) {
+      throw new Error('No comparator detected in ' + path);
+    }
+
+    return _.camelCase(comparatorInSchema);
+  }
+
+  _formatStrArray(strArr) {
     return _.map(_.compact(strArr), (x) => x.trim());
   }
 
